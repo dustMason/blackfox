6147ca83cd18c566a7344c83091c903c01ddb72e
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2013-07-28 23:00:09 -0600

    improvements. see readme.

--- download.py
+++ download.py
@@ -1,9 +1,21 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
 import urllib
 import urllib2
+import os
 
 from BeautifulSoup import BeautifulSoup as bs
 
 MAX_PAGES = 50
+HEADERS = {
+    "User-Agent" : "Mozilla/5.0",
+    "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language" : "en-US,en;q=0.8",
+    "Cache-Control" : "no-cache",
+    "Connection" : "keep-alive",
+    "Pragma" : "no-cache"
+}
 
 
 def get_backgrounds():
@@ -16,15 +28,22 @@ def get_images_from_page(url):
     html = fetchurl(url)
     soup = bs(html)
     for link in soup.findAll('a'):
-        href = link['href']
-        if '-1440x900' in href:
-            print 'Downloading %s' % href
-            urllib.urlretrieve(href, 'fox_backgrounds%s' % href[href.rfind('/'):])
+        try:
+            href = link['href']
+            if '-1680x1050' in href:
+                print 'Downloading %s' % href
+                f = fetchurl(href)
+                with open('fox_backgrounds%s' % href[href.rfind('/'):], "wb") as local_file:
+                    local_file.write(f)
+        except KeyError:
+            pass
 
 
 def fetchurl(url):
-    response = urllib2.urlopen(url)
-    return response.read()
+    request = urllib2.Request(url, headers = HEADERS)
+    response = urllib2.urlopen(request)
+    content = response.read()
+    return content
 
 
 if __name__ == '__main__':
